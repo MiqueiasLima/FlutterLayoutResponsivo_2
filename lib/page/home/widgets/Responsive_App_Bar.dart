32b871e8219f8d3746a11e060cf5619ec78d35fe
MiqueiasLima <miqueiaslima@outlook.com>
2021-06-24 17:24:11 -0300

    Finalizando barra de pesquisa

--- lib/page/home/widgets/Responsive_App_Bar.dart
+++ lib/page/home/widgets/Responsive_App_Bar.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class Responsive_App_Bar extends StatelessWidget {
@@ -14,12 +15,39 @@ class Responsive_App_Bar extends StatelessWidget {
         ),
         child: Row(
           children: <Widget>[
-            Text(
-              "Flutter",
-              style: TextStyle(
-                  fontSize: 30, color: Colors.white, fontFamily: 'Billabong',fontWeight: FontWeight.w500),
+            MouseRegion(
+              cursor: SystemMouseCursors.click,
+              child: Text(
+                "Flutter",
+                style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                    fontFamily: 'Billabong',
+                    fontWeight: FontWeight.w500),
+              ),
             ),
-
+            Container(
+              padding: EdgeInsets.only(left: 4),
+              width: 200,
+              height: 30,
+              alignment: Alignment.centerLeft,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.white),
+              ),
+              child: Row(children: <Widget>[
+                Icon(Icons.search,size: 15,),
+                const SizedBox(width: 4,),
+                Expanded(
+                    child: TextField(
+                  style: TextStyle(color: Colors.white, fontSize: 15),
+                  decoration: InputDecoration(
+                    border: InputBorder.none,
+                    isCollapsed: true,
+                  ),
+                )
+                ),
+              ]),
+            )
           ],
         ),
       ),
